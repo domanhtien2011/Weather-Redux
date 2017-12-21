a7ae745599de14957ff3128dbd03c3d80b6d5b9b
Tien Manh Do <domanhtien2011@gmail.com>
2017-12-21 18:30:28 +0700

    Adding charline for temperature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,47 +1,61 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import Chart from "../components/chart";
-import GoogleMap from "../components/google_map";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
+
   renderWeather(cityData) {
-    const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
-    const pressures = cityData.list.map(weather => weather.main.pressure);
-    const humidities = cityData.list.map(weather => weather.main.humidity);
-    const { lon, lat } = cityData.city.coord;
-
+    // console.log('Object keys: ', _.keys(cityData.list));
+    // console.log('Object values: ', _.values(cityData.list));
+    const temps = cityData.list.map((weather) => weather.main.temp
+    )
+    console.log(temps);
+    const cityName = cityData.city.name;
     return (
-      <tr key={name}>
-        <td><GoogleMap lon={lon} lat={lat} /></td>
-        <td><Chart data={temps} color="orange" units="K" /></td>
-        <td><Chart data={pressures} color="green" units="hPa" /></td>
-        <td><Chart data={humidities} color="black" units="%" /></td>
+      <tr key={cityName}>
+        <td>{cityName}</td>
+        <td>
+          <Sparklines height={120}
+            width={180}
+            data={temps}>
+            <SparklinesLine color='red' />
+          </Sparklines>
+        </td>
       </tr>
-    );
+    )
   }
-
   render() {
     return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (K)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
+      <div>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>
+                City
+              </th>
+              <th>
+                Temperature
+              </th>
+              <th>
+                Pressure
+              </th>
+              <th>
+                Humidity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.weather.map(this.renderWeather)}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
 
 function mapStateToProps({ weather }) {
-  return { weather };
+  return { weather } // same as {weather: weather}
 }
 
 export default connect(mapStateToProps)(WeatherList);
